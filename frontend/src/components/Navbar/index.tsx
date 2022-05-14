ce327f40bb7b43f7ffcaf170bf87b078712202f2
Ezequiel Almarone <ezequiel.almarone@gmail.com>
2022-05-14 09:14:41 -0400

    NavBar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import { ReactComponent as GithubIcon } from '../../assets/img/github.svg';
+import './styles.css';
+function Navbar() {
+    return(
+        <header>
+      <nav className='container'>
+        <div className='dsmovie-nav-content'>
+          <h1 className='dsmovie-title-contact'>DSMovie</h1>
+          <a href="https://github.com/EzequielAlmarone">
+            <div className='dsmovie-contact-container'>
+              <GithubIcon/>
+              <p className='dsmovie-contact-link'>/EzequielAlmarone</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+    );
+}
+export default Navbar;
